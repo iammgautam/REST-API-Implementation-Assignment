e0934ee0360a47144c93a7ddd542d9cdf1004f42
zengboi <mithileshgautam666@gmail.com>
2022-01-14 12:25:30 +0530

    Searchfilter & ordering filter added

--- user_app/views.py
+++ user_app/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import User
 from .serializers import WriteUserSerializer, ReadUserSerializer
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
+    filter_backends = (SearchFilter, OrderingFilter,)
+    search_fields = ("first_name", "last_name")
+    ordering_fields = ("age",)
 
     def get_serializer_class(self):
         if self.action in ("list", 'retrieve'):
