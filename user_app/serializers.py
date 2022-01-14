a0dd57f066342a3fca6f64dd9b041196fc7d5c06
zengboi <mithileshgautam666@gmail.com>
2022-01-14 12:16:33 +0530

    read/write serializer implemented for efficiency

--- user_app/serializers.py
+++ user_app/serializers.py
@@ -1,8 +1,36 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class WriteUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('__all__')
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "company_name",
+            "city",
+            "state",
+            "zip",
+            "email",
+            "web",
+            "age"
+        ) 
+
+class ReadUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "company_name",
+            "city",
+            "state",
+            "zip",
+            "email",
+            "web",
+            "age"
+        )
+        read_only_fields = fields
             
--- user_app/views.py
+++ user_app/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import WriteUserSerializer, ReadUserSerializer
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
+
+    def get_serializer_class(self):
+        if self.action in ("list", 'retrieve'):
+            return ReadUserSerializer
+        return WriteUserSerializer
